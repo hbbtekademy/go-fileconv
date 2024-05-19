24f372b6957193db65d19c704d188d9a270a5f99
HBB <h.bhate1805@gmail.com>
2024-05-19 19:15:47 +0530

    add docs

--- pkg/jsonparam/read_jsonparam.go
+++ pkg/jsonparam/read_jsonparam.go
@@ -31,6 +31,7 @@ const (
 	False       Records = "false"
 )
 
+// Parameters for reading a JSON file
 type ReadParams struct {
 	autodetect       bool
 	columns          map[string]string
@@ -68,96 +69,189 @@ const (
 	dfltUnionByName     bool        = false
 )
 
+/*
+Whether to auto-detect the names of the keys and data types of the values automatically.
+Default true
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithAutoDetect(autodetect bool) ReadParam {
 	return func(jp *ReadParams) {
 		jp.autodetect = autodetect
 	}
 }
 
+/*
+A map of key names and value types contained within the JSON file. (e.g., key1: 'INTEGER', key2: 'VARCHAR').
+If auto_detect is enabled these will be inferred.
+Default empty
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithColumns(columns map[string]string) ReadParam {
 	return func(jp *ReadParams) {
 		jp.columns = columns
 	}
 }
 
+/*
+The compression type for the file.
+By default this will be detected automatically from the file extension.
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithCompression(compression Compression) ReadParam {
 	return func(jp *ReadParams) {
 		jp.compression = compression
 	}
 }
 
+/*
+Whether strings representing integer values should be converted to a numerical type.
+Default false
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithConvStr2Int(convInt2Str bool) ReadParam {
 	return func(jp *ReadParams) {
 		jp.convStr2Int = convInt2Str
 	}
 }
 
+/*
+Specifies the date format to use when parsing dates.
+Default 'iso'
+
+https://duckdb.org/docs/sql/functions/dateformat
+*/
 func WithDateFormat(dateFormat string) ReadParam {
 	return func(jp *ReadParams) {
 		jp.dateformat = dateFormat
 	}
 }
 
+/*
+Whether or not an extra filename column should be included in the result.
+Default false
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithFilename(filename bool) ReadParam {
 	return func(jp *ReadParams) {
 		jp.filename = filename
 	}
 }
 
+/*
+Can be one of ('auto', 'unstructured', 'newline_delimited', 'array').
+Default 'auto'
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithFormat(format Format) ReadParam {
 	return func(jp *ReadParams) {
 		jp.format = format
 	}
 }
 
+/*
+Whether or not to interpret the path as a Hive partitioned path.
+Default false
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithHivePartitioning(hivePartitioning bool) ReadParam {
 	return func(jp *ReadParams) {
 		jp.hivePartitioning = hivePartitioning
 	}
 }
 
+/*
+Whether to ignore parse errors (only possible when format is 'newline_delimited').
+Default false
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithIgnoreErrors(ignoreErrors bool) ReadParam {
 	return func(jp *ReadParams) {
 		jp.ignoreErrors = ignoreErrors
 	}
 }
 
+/*
+Maximum nesting depth to which the automatic schema detection detects types.
+Set to -1 to fully detect nested JSON types.
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithMaxDepth(maxDepth int64) ReadParam {
 	return func(jp *ReadParams) {
 		jp.maxDepth = maxDepth
 	}
 }
 
+/*
+The maximum size of a JSON object (in bytes).
+Default 16777216 bytes
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithMaxObjSize(maxObjSize uint64) ReadParam {
 	return func(jp *ReadParams) {
 		jp.maxObjSize = maxObjSize
 	}
 }
 
+/*
+Can be one of ('auto', 'true', 'false').
+Default 'auto'
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithRecords(records Records) ReadParam {
 	return func(jp *ReadParams) {
 		jp.records = records
 	}
 }
 
+/*
+Option to define number of sample objects for automatic JSON type detection.
+Set to -1 to scan the entire input file
+Default 20480
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithSampleSize(sampleSize uint64) ReadParam {
 	return func(jp *ReadParams) {
 		jp.sampleSize = sampleSize
 	}
 }
 
+/*
+Specifies the date format to use when parsing timestamps.
+Default 'iso'
+
+https://duckdb.org/docs/sql/functions/dateformat
+*/
 func WithTimestampFormat(timestampformat string) ReadParam {
 	return func(jp *ReadParams) {
 		jp.timestampformat = timestampformat
 	}
 }
 
+/*
+Whether the schema’s of multiple JSON files should be unified.
+Default false
+
+https://duckdb.org/docs/data/json/overview#parameters
+*/
 func WithUnionByName(unionByName bool) ReadParam {
 	return func(jp *ReadParams) {
 		jp.unionByName = unionByName
 	}
 }
 
+// https://duckdb.org/docs/data/json/overview#parameters
 func NewReadParams(params ...ReadParam) *ReadParams {
 	jsonParams := &ReadParams{
 		autodetect:       dfltAutodetect,
@@ -184,6 +278,7 @@ func NewReadParams(params ...ReadParam) *ReadParams {
 	return jsonParams
 }
 
+// Returns formatted parameters for reading JSON file
 func (p *ReadParams) Params() string {
 	params := []string{}
 
