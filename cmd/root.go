b29872208882b3c8d69a10902a8adb2466a397ea
HBB <h.bhate1805@gmail.com>
2024-05-21 22:08:16 +0530

    refactoring

--- cmd/root.go
+++ cmd/root.go
@@ -42,11 +42,18 @@ func Execute() {
 }
 
 func init() {
+	cobra.EnableCommandSorting = false
+	registerGlobalFlags(rootCmd)
+}
+
+func registerGlobalFlags(rootCmd *cobra.Command) {
 	rootCmd.PersistentFlags().String(PQ_COMPRESSION, "snappy", "(Optional) The compression type for the output parquet file.")
 	rootCmd.PersistentFlags().StringSlice(PQ_PARTITION_BY, []string{}, "(Optional) Write to a Hive partitioned data set of Parquet files.")
-	rootCmd.PersistentFlags().String(PQ_FILENAME_PATTERN, "data_{i}.parquet", "(Optional) With this flag a pattern with {i} or {uuid} can be defined to create specific partition filenames.")
 	rootCmd.PersistentFlags().Bool(PQ_OVERWRITE_OR_IGNORE, false, "(Optional) Use this flag to allow overwriting an existing directory.")
+	rootCmd.PersistentFlags().String(PQ_FILENAME_PATTERN, "data_{i}.parquet", "(Optional) With this flag a pattern with {i} or {uuid} can be defined to create specific partition filenames.")
 	rootCmd.PersistentFlags().Bool(PQ_PER_THREAD_OUTPUT, false, "(Optional) If the final number of Parquet files is not important, writing one file per thread can significantly improve performance.")
+	rootCmd.PersistentFlags().SortFlags = false
+	rootCmd.Flags().SortFlags = false
 }
 
 func getPqWriteFlags(flags *pflag.FlagSet) (*pqWriteFlags, error) {
