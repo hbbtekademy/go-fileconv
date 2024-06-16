bced8c4525f87c956b19c02dd2e36a4c2e3dd842
HBB <h.bhate1805@gmail.com>
2024-06-16 18:59:20 +0530

    add flatten flag

--- pkg/fileconv/json2parquet.go
+++ pkg/fileconv/json2parquet.go
@@ -14,17 +14,28 @@ import (
 func (c *fileconv) Json2Parquet(ctx context.Context, srcJson string, dest string, pqWriteParams *pqparam.WriteParams, jsonParams ...jsonparam.ReadParam) error {
 	jsonReadParams := jsonparam.NewReadParams(jsonParams...)
 
-	_, err := c.db.ExecContext(ctx, fmt.Sprintf(`
+	if !jsonReadParams.GetFlatten() {
+		_, err := c.db.ExecContext(ctx, fmt.Sprintf(`
 		COPY (
 			SELECT * FROM read_json('%s' %s)
 		) TO '%s' %s`,
-		srcJson,
-		jsonReadParams.Params(),
-		dest,
-		pqWriteParams.Params()))
-	if err != nil {
-		return err
+			srcJson,
+			jsonReadParams.Params(),
+			dest,
+			pqWriteParams.Params()))
+		if err != nil {
+			return err
+		}
+		return nil
 	}
+
+	// Import json into temp table
+
+	// describe table
+
+	// For every struct column flatten it
+
+	// COPY (select with column alias (select with unnest for struct columns)) to parquet
 
 	return nil
 }
